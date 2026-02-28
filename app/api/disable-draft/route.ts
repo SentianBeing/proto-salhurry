9de60a08fd5b621be1533440abb72ec24a1fb0ed
『TeMpest ꧂ <94009878+SentianBeing@users.noreply.github.com>
2026-03-01 00:23:29 +0530

    fix: make draftMode awaited as required by Next 15

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -2,7 +2,8 @@ import { draftMode } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export async function GET(request: Request) {
-    draftMode().disable();
+    const draft = await draftMode();
+    draft.disable();
     // Redirect to the home page after disabling draft mode
     redirect('/');
 }
